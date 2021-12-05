91f60ce3a23a17d5b399d2e47bef3784dedf30ef
debbie525 <57734103+debbie525@users.noreply.github.com>
2021-12-05 15:02:15 +0800

    Create BMI_Calculator.py
    Calculate the Body Mass Index using weight in kilograms and height in meters.
    This is the formula:
    BMI=weight(kg)/height**2

---
+++ BMI_Calculator.py
@@ -0,0 +1,15 @@
+
+# BMI Calculator
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+BMI = float(weight)/(float(height)**2)
+print('Your BMI is: ' + str(int(BMI)))
+
+
+
+
+
+
+
+
