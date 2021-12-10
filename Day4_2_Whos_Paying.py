a193ab6fad848cd463ad1879803a80380988da1d
debbie525 <57734103+debbie525@users.noreply.github.com>
2021-12-10 21:14:08 +0800

    Update Day4_2_Whos_Paying.py
    update line 16 and 18

--- Day4_2_Whos_Paying.py
+++ Day4_2_Whos_Paying.py
@@ -1,6 +1,11 @@
 '''
-You are going to write a program which will select a random name from a list of names. 
+Banker Roulette
+
+Write a program which will select a random name from a list of names. 
 The person selected will have to pay for everybody's food bill.
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
 
 '''
 import random
@@ -8,10 +13,8 @@ names_string = input("Give me everybody's names separated by a comma.\n")
 
 names_list = names_string.split(",")
 
-print(names_list)
+selected_index = random.randint(0, len(names_list)-1)    # take note of this len() 
 
-selected_index = random.randint(0, len(names_list))
-
-selected_name = names_list[selected_index].title()  # use title to capitalize the first letter
+selected_name = names_list[selected_index].title()  # title()- capitalize the first letter
 
 print(f"{selected_name} will going to buy the meal today!")
