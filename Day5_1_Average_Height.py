37cc9121963c4ab74bedff8b1973638ef73da439
debbie525 <57734103+debbie525@users.noreply.github.com>
2021-12-19 17:22:48 +0800

    Update Day5_1_Average_Height.py
    update variable name

--- Day5_1_Average_Height.py
+++ Day5_1_Average_Height.py
@@ -1,17 +1,17 @@
 '''
-Write a program that calculates the average student height from List of heights using for loop
+Write a program that calculates the average student height from List of heights using "for loop"
 
 '''
 
 height_list = input ("Input a list of students heights, separated by comma:\n").split(",")
 
 total_height = 0
-counter = 0
+number_of_students = 0
 for height in height_list:
     total_height += int(height)
-    counter += 1
+    number_of_students += 1
 
-average_height = total_height/counter
+average_height = total_height/number_of_students
 round_average_height = round(average_height)
 
 
