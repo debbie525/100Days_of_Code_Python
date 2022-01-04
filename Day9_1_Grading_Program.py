86fe83d0953900971ac5e2e98058b8908c697948
debbie525 <57734103+debbie525@users.noreply.github.com>
2022-01-04 18:06:04 +0800

    Create Day9_1_Grading_Program.py
    create a new dictionary for grades category

---
+++ Day9_1_Grading_Program.py
@@ -0,0 +1,45 @@
+''''
+You have access to a database of student_scores in the format of a dictionary. The keys in student_scores are the names of the students and the values are their exam scores.
+
+Write a program that converts their scores to grades. By the end of your program, you should have a new dictionary called student_grades that should contain student names for keys and their grades for values. The final version of the student_grades dictionary will be checked.
+
+This is the scoring criteria:
+
+Scores 91 - 100: Grade = "Outstanding"
+
+Scores 81 - 90: Grade = "Exceeds Expectations"
+
+Scores 71 - 80: Grade = "Acceptable"
+
+Scores 70 or lower: Grade = "Fail"
+
+
+
+
+'''
+
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+student_grades = {}
+
+for name in student_scores:
+    if student_scores[name] <=70:
+        category = "Fail"
+    elif student_scores[name]>=71 and student_scores[name]<=80:
+        category = "Acceptable"
+    elif student_scores[name]>=81 and student_scores[name]<=90:
+        category = "Exceeds Expectations"
+    elif student_scores[name]>=91 and student_scores[name]<=100:
+        category = "Outstanding"
+
+    student_grades[name] = category
+
+
+print(student_grades)
