b14805ab72264a5dbff4573c781407b9c8e1e190
debbie525 <57734103+debbie525@users.noreply.github.com>
2022-01-08 16:07:36 +0800

    Update Day4_2_Whos_Paying.py
    update comments

--- Day4_2_Whos_Paying.py
+++ Day4_2_Whos_Paying.py
@@ -1,13 +1,10 @@
-'''
-Banker Roulette
 
-Write a program which will select a random name from a list of names. 
-The person selected will have to pay for everybody's food bill.
+# Banker Roulette
 
-test_seed = int(input("Create a seed number: "))
-random.seed(test_seed)
+# Write a program which will select a random name from a list of names. 
+# The person selected will have to pay for everybody's food bill.
 
-'''
+
 import random
 names_string = input("Give me everybody's names separated by a comma.\n")
 
