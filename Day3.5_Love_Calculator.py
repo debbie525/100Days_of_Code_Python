1a175d7ff4b67faa2b877fafad09f79fb54e5c84
debbie525 <57734103+debbie525@users.noreply.github.com>
2022-01-08 16:06:33 +0800

    Update Day3.5_Love_Calculator.py
    update comments

--- Day3.5_Love_Calculator.py
+++ Day3.5_Love_Calculator.py
@@ -1,29 +1,29 @@
-'''
-You are going to write a program that tests the compatibility between two people.
-To work out the love score between two people:
-Take both people's names and check for the number of times the letters in the word TRUE occurs. 
-Then check for the number of times the letters in the word LOVE occurs. 
-Then combine these numbers to make a 2 digit number.
 
-For Love Scores less than 10 or greater than 90, the message should be:
-"Your score is **x**, you go together like coke and mentos."
+# You are going to write a program that tests the compatibility between two people.
+# To work out the love score between two people:
+# Take both people's names and check for the number of times the letters in the word TRUE occurs. 
+# Then check for the number of times the letters in the word LOVE occurs. 
+# Then combine these numbers to make a 2 digit number.
 
-For Love Scores between 40 and 50, the message should be:
-"Your score is **y**, you are alright together."
+# For Love Scores less than 10 or greater than 90, the message should be:
+# "Your score is **x**, you go together like coke and mentos."
 
-Otherwise, the message will just be their score. e.g.:
-"Your score is **z**."
+# For Love Scores between 40 and 50, the message should be:
+# "Your score is **y**, you are alright together."
 
-Example:
+# Otherwise, the message will just be their score. e.g.:
+# "Your score is **z**."
 
-Input:
-name1 = "Kanye West"
-name2 = "Kim Kardashian"
+# Example:
 
-Output:
-Your score is 42, you are alright together.
+# Input:
+# name1 = "Kanye West"
+# name2 = "Kim Kardashian"
 
-'''
+# Output:
+# Your score is 42, you are alright together.
+
+
 
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name?\n")
