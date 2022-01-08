273a28c8c0fae9b79c1a10acb3ccb92af32a50fa
debbie525 <57734103+debbie525@users.noreply.github.com>
2022-01-08 16:04:18 +0800

    Update Day3_2_BMI_Calculator_2.0.py
    update comments

--- Day3_2_BMI_Calculator_2.0.py
+++ Day3_2_BMI_Calculator_2.0.py
@@ -1,17 +1,16 @@
 
-'''
-Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+# Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
 
-It should tell them the interpretation of their BMI based on the BMI value.
+# It should tell them the interpretation of their BMI based on the BMI value.
 
-- Under 18.5 they are underweight
-- Over 18.5 but below 25 they have a normal weight
-- Over 25 but below 30 they are slightly overweight
-- Over 30 but below 35 they are obese
-- Above 35 they are clinically obese.
+# - Under 18.5 they are underweight
+# - Over 18.5 but below 25 they have a normal weight
+# - Over 25 but below 30 they are slightly overweight
+# - Over 30 but below 35 they are obese
+# - Above 35 they are clinically obese.
 
-Note: you should **round** the result to the nearest whole number
-'''
+# Note: you should **round** the result to the nearest whole number
+
 
 
 # BMI Calculator
