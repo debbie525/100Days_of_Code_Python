4232683537994d2a89412c407ba50b5650592ef8
debbie525 <57734103+debbie525@users.noreply.github.com>
2022-01-08 10:55:23 +0800

    Determine the number of days in a given month
    Determine the number of days in a given   month. User inputs will be year and month. You must consider if a year is a leap year or not.

--- scratch.py
+++ scratch.py
@@ -1,11 +1,8 @@
-print(18%5)
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
-'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
-'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
-'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
-'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-]
 
-for item, index in enumerate(alphabet):
-    print (item, index)
+def format_name(f_name, l_name):
+    full_name = f_name + " " + l_name
+    return full_name.title()
+
+result=format_name(f_name="debbie", l_name="palon")
+print(result)
