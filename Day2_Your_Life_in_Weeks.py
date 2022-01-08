611d2f29878c5a01b2a42d1b775296282ffa6604
debbie525 <57734103+debbie525@users.noreply.github.com>
2022-01-08 16:03:17 +0800

    Update Day2_Your_Life_in_Weeks.py
    update comments

--- Day2_Your_Life_in_Weeks.py
+++ Day2_Your_Life_in_Weeks.py
@@ -1,21 +1,20 @@
-'''
-Day 2-3 Exercise
 
-date: December 5, 2021
+# Day 2-3 Exercise
 
-Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
-It will take your current age as the input and output a message with our time left in this format:
-You have x days, y weeks, and z months left.
-Where x, y and z are replaced with the actual calculated numbers.
-Warning your output should match the Example Output format exactly, even the positions of the commas and full stops.
+# date: December 5, 2021
+
+# Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+# It will take your current age as the input and output a message with our time left in this format:
+# You have x days, y weeks, and z months left.
+# Where x, y and z are replaced with the actual calculated numbers.
+# Warning your output should match the Example Output format exactly, even the positions of the commas and full stops.
+
+# 1 year = 365 days
+# 1 year = 52 weeks
+# 1 year = 12 months
 
 
-1 year = 365 days
-1 year = 52 weeks
-1 year = 12 months
 
-
-'''
 age = input("What is your current age?")
 
 final_age_day = 90*365
