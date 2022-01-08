ce066070e68d1737f280daedeaa6cef9b2b60746
debbie525 <57734103+debbie525@users.noreply.github.com>
2022-01-08 16:05:38 +0800

    Update Day3_4_Pizza_Order.py
    update comments

--- Day3_4_Pizza_Order.py
+++ Day3_4_Pizza_Order.py
@@ -1,15 +1,14 @@
-'''
-Build an automatic pizza order program.
-Based on a user's order, work out their final bill.
 
-Small Pizza: $15
-Medium Pizza: $20
-Large Pizza: $25
-Pepperoni for Small Pizza: +$2
-Pepperoni for Medium or Large Pizza: +$3
-Extra cheese for any size pizza: + $1
+# Build an automatic pizza order program.
+# Based on a user's order, work out their final bill.
 
-'''
+# Small Pizza: $15
+# Medium Pizza: $20
+# Large Pizza: $25
+# Pepperoni for Small Pizza: +$2
+# Pepperoni for Medium or Large Pizza: +$3
+# Extra cheese for any size pizza: + $1
+
 
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
